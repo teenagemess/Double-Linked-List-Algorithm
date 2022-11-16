3ff85fb512515c98d8547bfea1873f770a002d04
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-11-16 19:18:53 +0700

    membuat function Main

--- Double Linked List Algorithm/Program.cs
+++ Double Linked List Algorithm/Program.cs
@@ -139,5 +139,100 @@ namespace Double_Linked_List_Algorithm
                 }
             }
         }
+        public bool listEmpty()
+        {
+            if (START == null)
+                return true;
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            DoubleLinkedLists obj = new DoubleLinkedLists();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\n Menu" +
+                        "\n 1. Add a record to the list" +
+                        "\n 2. Delete a record from the list" +
+                        "\n 3. View all records in the ascending order of roll numbers" +
+                        "\n 4. View all records in the descending order of roll numbers" +
+                        "\n 5. Search for a record in the list" +
+                        "\n 6. Exit \n" +
+                        "\n Enter your choice (1-6): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.addNode();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Console.Write("\n Enter the roll number of the student" +
+                                    "whose record is to be deleted: ");
+                                int rollNo = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                if (obj.delNode(rollNo) == false)
+                                    Console.WriteLine("Record not found");
+                                else
+                                    Console.WriteLine("Record with roll number " + rollNo + " deleted \n");
+                            }
+                            break;
+                        case '3':
+                            {
+                                obj.traverse();
+                            }
+                            break;
+                        case '4':
+                            {
+                                obj.revtraverse();
+                            }
+                            break;
+                        case '5':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.Write("\nEntere the roll number of the student whose record you want to search: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number: " + curr.rollNumber);
+                                    Console.WriteLine("\nName: " + curr.name);
+                                }
+                            }
+                            break;
+                        case '6':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("\nInvalid option");
+                            }
+                            break;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.Write("Check for the values entered.");
+                }
+                
+            }
+        }
     }
 }
