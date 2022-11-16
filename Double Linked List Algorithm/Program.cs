a9735eace5c8d4111ab15ef179d97f8ba66d3e36
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-11-16 17:03:53 +0700

    membuat class node

--- Double Linked List Algorithm/Program.cs
+++ Double Linked List Algorithm/Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace Double_Linked_List_Algorithm
 {
-    internal class Program
+    class Node
     {
-        static void Main(string[] args)
-        {
-        }
+        /*Node class represent the node of doubly linked list.
+         * It consists of the information part and links to
+         * its succeeding and preceeding nodes
+         * in terms of the next and previous nodes. */
+        public int rollNumber;
+        public string name;
+        public Node next; //points to the succeeding node
+        public Node prev; //points to the preceeding node
     }
 }
